697533941e554fb07e2d4fee9f95da6a0c047047
Laurens Duijvesteijn <git@duijf.io>
2024-05-31 10:34:34 +0200

    Remove `CNAME` from `gh-pages` branch

--- bin/upload-docs.sh
+++ bin/upload-docs.sh
@@ -6,6 +6,8 @@ rye run sphinx-build --write-all $PROJECT_ROOT/documentation $PROJECT_ROOT/docs
 touch $PROJECT_ROOT/docs/.nojekyll
 git checkout -b gh-pages
 git add -f docs
+rm CNAME
+git add CNAME
 git commit -m "Build docs"
 git push -f origin gh-pages
 git checkout -
