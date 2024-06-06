c2804f28183011961b49588a56cb717a20fa51cb
Laurens Duijvesteijn <git@duijf.io>
2024-06-06 14:33:16 +0200

    Apparently we do need the `CNAME`?

--- bin/upload-docs.sh
+++ bin/upload-docs.sh
@@ -6,8 +6,6 @@ rye run sphinx-build --write-all $PROJECT_ROOT/documentation $PROJECT_ROOT/docs
 touch $PROJECT_ROOT/docs/.nojekyll
 git checkout -b gh-pages
 git add -f docs
-rm CNAME
-git add CNAME
 git commit -m "Build docs"
 git push -f origin gh-pages
 git checkout -
